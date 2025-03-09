f1534104d7f027104221f507f83772e97a1604b5
Dodo-tech001 <ajiboladavid07@gmail.com>
2025-03-09 10:55:20 +0100

    Update ALLOWED_HOSTS and settings for Render

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h&3ww_opql*hjdh=$i4n#ffo@^(^ksav_k!_wb^26bxo21n#y'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
 
 
 # Application definition
